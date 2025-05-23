607d29d7601acd5ad780199a10d720a762a06eed
Leonardo Bragatti <leonardo.bragatti@gmail.com>
2025-05-23 15:00:47 -0300

    Testando code rabbit

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import HomeTab from "@/Navigation/Home";
 
 export default function App() {
+  const nome = "Leonardo";
   return (
     <NavigationContainer>
       <HomeTab />
